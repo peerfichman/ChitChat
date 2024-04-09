7f74b56fa419de454973d272e2e4287ccb50555f
Peer <peerfichman@gmail.com>
2024-04-09 13:55:03 +0300

    finshed chat tutorial

--- Chat/Front/chitchat_chat/src/App.js
+++ Chat/Front/chitchat_chat/src/App.js
@@ -1,24 +1,17 @@
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ChatBox from "./components/ChatBox";
 import Welcome from "./components/Welcome";
-import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState(false);
-
-  return (
-    <div className="App">
-      <NavBar />
-      {!user ? (
-        <Welcome />
-      ) : (
-        <>
-          <ChatBox />
-        </>
-      )}
-    </div>
-  );
+    const [user] = useAuthState(auth);
+    return (
+        <div className="App">
+            <NavBar />
+            {!user ? <Welcome /> : <ChatBox />}
+        </div>
+    );
 }
-
 export default App;
