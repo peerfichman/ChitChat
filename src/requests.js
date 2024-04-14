c6101cc463462d9abb8003c48f86e644e8d98dec
DorHaimovich12 <Dor.haimovich@stu.shenkar.ac.il>
2024-04-14 18:16:55 +0300

    feat/ create-experiment

--- src/requests.js
+++ src/requests.js
@@ -1,22 +1,26 @@
 import axios from 'axios';
+import { statusOptions } from './constant';
+const baseURL = process.env.REACT_APP_CHICHAT_API_URL;
 
 const getAllExperiments = async () => {
+    const URL = baseURL + 'experiments';
     try {
-        const URL = process.env.REACT_APP_CHICHAT_API_URL + 'experiments';
         const response = await axios.get(URL);
         return response.data;
     } catch (error) {
         console.error('Failed to fetch experiments', error);
+        return null;
     }
 };
 
 const getExperimentById = async (id) => {
+    const URL = baseURL + `experiments/${id}`;
     try {
-        const URL = process.env.REACT_APP_CHICHAT_API_URL + `experiments/${id}`;
         const response = await axios.get(URL);
         return response.data[0];
     } catch (error) {
         console.error('Failed to fetch experiment', error);
+        return null;
     }
 };
 
@@ -27,13 +31,51 @@ const updateExperimentStatus = async (experiment, status) => {
         exp_status: status,
         exp_subject: experiment.exp_subject,
     };
+    const URL = baseURL + `experiments`;
     try {
-        const URL = process.env.REACT_APP_CHICHAT_API_URL + `experiments`;
         const response = await axios.put(URL, newExperiment);
         return response.data;
     } catch (error) {
         console.error('Failed to update experiment status', error);
+        return null;
     }
 };
 
-export { getAllExperiments, getExperimentById, updateExperimentStatus };
+const createExperiment = async (experiment) => {
+    const URL = baseURL + 'experiments';
+    try {
+        const response = await axios.post(URL, {
+            exp_subject: experiment.expSubject,
+            exp_prompt: experiment.expPrompt,
+            exp_status: statusOptions.NOT_STARTED,
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Failed to create experiment', error);
+        return null;
+    }
+};
+
+const createAgent = async (agent, experimentID) => {
+    const URL = baseURL + 'agents';
+    try {
+        const response = await axios.post(URL, {
+            agent_name: agent.name,
+            agent_sentiment: agent.sentiment,
+            agent_eng: 0.25,
+            exp_id: experimentID,
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Failed to create agent', error);
+        return null;
+    }
+};
+
+export {
+    getAllExperiments,
+    getExperimentById,
+    updateExperimentStatus,
+    createExperiment,
+    createAgent,
+};
