589dfb28a2b93217e770019992d4dc88dd50c270
DorHaimovich12 <Dor.haimovich@stu.shenkar.ac.il>
2024-04-14 17:23:22 +0300

    feat/ added axios

--- src/components/Experiment/ExperimentCard.js
+++ src/components/Experiment/ExperimentCard.js
@@ -8,7 +8,6 @@ const ExperimentCard = ({ experiment }) => {
         <div
             className="flex flex-col items-center bg-gray-50 hover:bg-gray-200 border shadow-sm rounded-xl p-4 w-[300px] gap-3  cursor-pointer"
             onClick={() => navigate(`/experiments/${experiment.exp_id}`)}>
-            
             <h3 className="text-2xl font-bold text-gray-800 ">
                 {experiment.exp_name}
             </h3>
