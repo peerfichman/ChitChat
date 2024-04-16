b2b5c73f6091d2282533d214106fe51b3dcd9782
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2024-04-17 00:03:13 +0300

    experiment statistics + CSV download button

--- src/components/Metrics/GraphComponent.js
+++ src/components/Metrics/GraphComponent.js
@@ -1,51 +1,55 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect,  useState} from 'react';
 import Graph from "react-graph-vis";
 import {getNeo4jGraph} from "../../requests";
+import { v4 as uuidv4 } from 'uuid'
 
 const GraphComponent = ({ id }) => {
     const [graph, setGraph] = useState({ nodes: [], edges: [] });
+    const [isEmpty, setIsEmpty] = useState(true);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await getNeo4jGraph(id);
-                const nodesMap = new Map; // Use Map instead of Set
-                const links = [];
-                response.records.forEach(record => {
-                    const node1 = record._fields[record._fieldLookup.p];
-                    const node2 = record._fields[record._fieldLookup.q];
-                    const relationship = record._fields[record._fieldLookup.r];
+        getNeo4jGraph(id).then(response => {
+            const nodesMap = new Map; // Use Map instead of Set
+            const links = [];
+            response.records.forEach(record => {
+                const node1 = record._fields[record._fieldLookup.p];
+                const node2 = record._fields[record._fieldLookup.q];
+                // const relationship = record._fields[record._fieldLookup.r];
 
-                    const node1Id = `${node1.identity.low}_${node1.identity.high}`;
-                    const node2Id = `${node2.identity.low}_${node2.identity.high}`;
+                const node1Id = `${node1.identity.low}_${node1.identity.high}`;
+                const node2Id = `${node2.identity.low}_${node2.identity.high}`;
 
-                    // Store or update node information in Map by id
-                    if (!nodesMap.has(node1Id)) {
-                        nodesMap.set(node1Id, { id: node1Id, name: node1.properties.name });
-                    }
-                    if (!nodesMap.has(node2Id)) {
-                        nodesMap.set(node2Id, { id: node2Id, name: node2.properties.name });
-                    }
+                // Store or update node information in Map by id
+                if (!nodesMap.has(node1Id)) {
+                    nodesMap.set(node1Id, { id: node1Id, name: node1.properties.name });
+                }
+                if (!nodesMap.has(node2Id)) {
+                    nodesMap.set(node2Id, { id: node2Id, name: node2.properties.name });
+                }
 
-                    // Add link
-                    links.push({ from: node1Id, to: node2Id });
-                });
+                // Add link
+                links.push({ from: node1Id, to: node2Id });
+            });
 
-                const nodeArray = Array.from(nodesMap.values());
+            const nodeArray = Array.from(nodesMap.values());
 
-                console.log(nodeArray);
-                console.log(links);
-                // Convert Map values to an array for the nodes
-                setGraph({
-                    nodes: nodeArray,
-                    edges: links
-                });
-            } catch (error) {
-                console.error('Failed to fetch graph data:', error);
-            }
-        };
+            console.log(nodeArray);
+            console.log(links);
+            // Convert Map values to an array for the nodes
+            setGraph({
+                nodes: nodeArray,
+                edges: links
+            });
+            setIsEmpty(false);
 
-        fetchData().then(r => console.log(graph.edges));
+            console.log(graph.edges , graph.nodes);
+        } )
+            .catch(e => {
+                console.error('Failed to fetch graph data:', e);
+            })
+
+
+
 
     }, [id]); // Re-fetch when id changes
 
@@ -97,11 +101,12 @@ const GraphComponent = ({ id }) => {
 
     const events = {
         select: function(event) {
-            var { nodes, edges } = event;
+            let { nodes, edges } = event;
         }
     };
     return (
-        <Graph style={{width: '100%', height: '100%'}}
+        !isEmpty && <Graph style={{width: '100%', height: '100%'}}
+            key={uuidv4()}
             graph={graph}
             options={options}
             events={events}
