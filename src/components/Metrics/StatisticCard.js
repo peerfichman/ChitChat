4d5c4e99181e10f869245ea8c538f0c6b699faf9
GavrieliYovel <100140349+GavrieliYovel@users.noreply.github.com>
2024-04-20 20:31:57 +0300

    Update StatisticCard.js

--- src/components/Metrics/StatisticCard.js
+++ src/components/Metrics/StatisticCard.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import Status from '../Status';
 const StatisticCard = ({ name, count }) => {
-
     return (
-        <div
-            className="flex flex-col items-center bg-gray-50 hover:bg-gray-200 border shadow-sm rounded-xl p-4 w-[300px] gap-3">
-            <h3 className="text-lg font-bold text-gray-800 ">
-                {name}
-            </h3>
+        <div className="flex flex-col items-center bg-gray-50 hover:bg-gray-200 border shadow-sm rounded-xl p-4 w-[300px] gap-3">
+            <h3 className="text-lg font-bold text-gray-800 ">{name.replace(/_/g, ' ')}</h3>
             <p className="mt-2 text-gray-500 ">{count}</p>
         </div>
     );
