d5f3b0a4a105de5838edc92db646a3b4bd9d73b2
GavrieliYovel <100140349+GavrieliYovel@users.noreply.github.com>
2024-04-20 20:31:15 +0300

    Update ExperimentStatistics.js

--- src/components/Metrics/ExperimentStatistics.js
+++ src/components/Metrics/ExperimentStatistics.js
@@ -1,17 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { getNeo4jGraph } from '../../requests/metric';
 import StatisticCard from './StatisticCard';
+import Graph  from 'graphology';
+import {density} from 'graphology-metrics/graph/density';
+import diameter from 'graphology-metrics/graph/diameter';
+import eccentricity from 'graphology-metrics/node/eccentricity';
+import betweennessCentrality from 'graphology-metrics/centrality/betweenness';
+import closenessCentrality from 'graphology-metrics/centrality/closeness';
+import { degreeCentrality } from 'graphology-metrics/centrality/degree';
 
+
+const averageCalc = (data) => {
+    const dataValues = Object.values(data);
+    const dataSum = dataValues.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return dataSum/dataValues.length;
+}
 const GraphStatistics = ({ id }) => {
+
     const [statistics, setStatistics] = useState({
         Nodes: 0,
         Edges: 0,
         Positive_Edges: 0,
         Negative_Edges: 0,
         Natural_Edges: 0,
-    });
+    })
 
     useEffect(() => {
+        const graph = new Graph({multi: true});
         const fetchData = async () => {
             try {
                 const response = await getNeo4jGraph(id);
@@ -21,18 +36,32 @@ const GraphStatistics = ({ id }) => {
                 statistics.Positive_Edges = 0;
                 statistics.Negative_Edges = 0;
                 statistics.Natural_Edges = 0;
+                statistics.Diameter = 0;
+                statistics.Radius = 0;
+                statistics.Density = 0;
+                statistics.Self_Loops = 0;
+                statistics.Avg_Betweenness_Centrality = 0;
+                statistics.Avg_Closeness_Centrality = 0;
+                statistics.Avg_Degree_Centrality = 0;
                 response.records.forEach((record) => {
                     const node1 = record._fields[record._fieldLookup.p];
                     const node2 = record._fields[record._fieldLookup.q];
                     const relationship = record._fields[record._fieldLookup.r];
 
-                    const node1Id = `${node1.identity.low}_${node1.identity.high}`;
-                    const node2Id = `${node2.identity.low}_${node2.identity.high}`;
+                    const node1Id = `${node1.identity.low}${node1.identity.high}`;
+                    const node2Id = `${node2.identity.low}${node2.identity.high}`;
 
-                    // Add nodes to set for unique counting
-                    nodesSet.add(node1Id);
-                    nodesSet.add(node2Id);
+                    if(!nodesSet.has(node1Id)) {
+                        graph.addNode(node1Id);
+                        nodesSet.add(node1Id);
+                    }
 
+                    if(!nodesSet.has(node2Id)) {
+                        graph.addNode(node2Id);
+                        nodesSet.add(node2Id);
+                    }
+
+                    graph.addEdge(node1Id, node2Id);
                     statistics.Edges += 1;
 
                     // Count positive, negative, and natural edges
@@ -45,14 +74,33 @@ const GraphStatistics = ({ id }) => {
                         statistics.Natural_Edges += 1;
                     }
                 });
+                let radius = eccentricity(graph, Array.from(nodesSet)[0]);
+                nodesSet.forEach(node => {
+                    let currRadius = eccentricity(graph, node);
+                    if(currRadius < radius){
+                        radius = currRadius;
+                    }
+                })
+
+                const betweennessCentralisesAvg = averageCalc(betweennessCentrality(graph, {getEdgeWeight: null}));
+                const closenessCentralisesAvg  = averageCalc(closenessCentrality(graph));
+                const degreeCentralisesAvg = averageCalc(degreeCentrality(graph));
+
 
                 // Update statistics state
                 setStatistics({
-                    Nodes: nodesSet.size,
-                    Edges: statistics.Edges,
+                    Nodes: graph.order,
+                    Edges: graph.size,
                     Positive_Edges: statistics.Positive_Edges,
                     Negative_Edges: statistics.Negative_Edges,
                     Natural_Edges: statistics.Natural_Edges,
+                    Self_Loops: graph.selfLoopCount,
+                    Diameter: diameter(graph),
+                    Radius: radius,
+                    Density: density(graph),
+                    Avg_Betweenness_Centrality: betweennessCentralisesAvg,
+                    Avg_Closeness_Centrality: closenessCentralisesAvg,
+                    Avg_Degree_Centrality: degreeCentralisesAvg
                 });
             } catch (error) {
                 console.error('Failed to fetch graph data:', error);
