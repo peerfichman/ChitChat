8b29a978aebd0eb758b33c29f33b09e412353735
DorHaimovich <Dorhaim765@gmail.com>
2024-05-21 13:27:22 +0300

    chore: Remove unused npm dependency and export in utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,9 +2,4 @@ const getDateFormatted = (date) => {
     return new Date(date).toLocaleDateString('he-IL').replace(/\./g, '/');
 };
 
-const convertToCSV = (data) => {
-    const csvContent = '\uFEFF' + data.map((row) => row.join(',')).join('\n');
-    return 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
-};
-
-export { getDateFormatted, convertToCSV };
+export { getDateFormatted };
